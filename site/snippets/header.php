23556408068581bf58d7429f290bc5371ae4e041
August W. Miller <hello@gusmiller.com>
2015-02-27 00:06:20 -0800

    Header wizardry

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,9 +5,10 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
+  <title><?= $site->title()->html() ?></title>
   <meta name="description" content="<?= $site->description()->html() ?>">
   <meta name="keywords" content="<?= $site->keywords()->html() ?>">
+  <meta name="author" content="<?= $site->author() ?>">
 
   <?= css('assets/css/awm.css') ?>
   <?#= css('http://fonts.googleapis.com/css?family=Fira+Mono:400,700') ?>
@@ -19,7 +20,8 @@
   <header>
     <article>
       <div class="column full">
-        <h1>August Winfield Miller</h1>
+        <h1>
+          <?= html::a(url(), $site->author()->html()) ?>
       </div>
     </article>
   </header>
