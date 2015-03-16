f44151f00ac7465825d63725e4f7e4678386a817
August W. Miller <hello@gusmiller.com>
2015-03-15 22:36:25 -0700

    Improved error page

--- site/templates/error.php
+++ site/templates/error.php
@@ -1,10 +1,16 @@
 <? snippet('header') ?>
-<main>
-  <article>
-    <div class="column full">
-      <h1><?= $page->generic_error_title() ?></h1>
-      <?= $page->text()->kirbytext() ?>
-    </div>
-  </article>
-</main>
+
+  <main>
+    <article>
+      <div class="column full">
+        <h1><?= $page->generic_error_title() ?></h1>
+        <?= $page->text()->kirbytext() ?>
+      </div>
+    </article>
+
+    <article>
+      <div class="column full spacer"></div>
+    </article>
+  </main>
+  
 <? snippet('footer') ?>
